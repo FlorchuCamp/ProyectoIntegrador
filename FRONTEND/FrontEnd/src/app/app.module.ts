12f0012de47ee515ec9feff92fb0ec4ed4c2e400
FlorchuCamp <fflorchucamp@gmail.com>
2022-07-22 21:52:33 -0300

    ...

--- FRONTEND/FrontEnd/src/app/app.module.ts
+++ FRONTEND/FrontEnd/src/app/app.module.ts
@@ -6,13 +6,25 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillComponent } from './components/skill/skill.component';
+import { ProjectComponent } from './components/project/project.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavComponent,
-    BannerComponent
+    BannerComponent,
+    AboutMeComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillComponent,
+    ProjectComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
